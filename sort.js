d2131304644aac95541d4e0fc149bbe92be77859
Cam Pedersen <diffference@gmail.com>
2012-05-27 18:37:16 -0400

    insertion sort!

--- sort.js
+++ sort.js
@@ -19,3 +19,12 @@ Sort.Selection = function (arr) {
   }
   return arr;
 };
+
+Sort.Insertion = function (arr) {
+  var n = arr.length;
+  for (var i = 0; i < n; i++) {
+    for (var j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
+      this.exchange(arr, j, j - 1);
+    }
+  }
+};
